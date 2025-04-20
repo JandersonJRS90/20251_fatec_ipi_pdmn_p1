793b56271ab6111a15f2491b6086d17b60310f2e
Janderson Souza <janderson.jrs.90@gmail.com>
2025-04-20 13:30:20 -0300

    feat(p1): integra com a api da viacep

--- exibe-logradouro/src/App.jsx
+++ exibe-logradouro/src/App.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
 import Busca from './Componentes/Busca'
 import LocalidadeLista from './Componentes/LocalidadeLista'
+import axios from 'axios';
 
 class App extends React.Component {
 
-  onBuscaRealizada = (cep) => {
-    alert(cep)
+  onBuscaRealizada = async (cep) => {
+    cep = cep.replace(/\D/g, '');
+    if(cep != ""){
+      const validacep = /^[0-9]{8}$/;
+      if(validacep.test(cep)){
+        //alert(cep)
+        try {
+          const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
+          console.log(response.data);
+        } catch (error) {
+          console.error('Erro encontrado:', error);
+        }
+      }
+      else{
+        alert("Valor digitado não corresponde a um CEP")
+      }
+    }
+    else{
+      alert("Valor digitado não corresponde a um CEP")
+    }
   }
   render(){
     return (
